6c9aed0f59409c341cbaf07798be7dd20d1ca62b
SeonAh-Yoo <ysa9171@postech.ac.kr>
2022-07-25 22:14:02 -0400

    completely finished

--- add-nbo.c
+++ add-nbo.c
@@ -1,44 +1,53 @@
 #include <stddef.h> // for size_t
-#include <stdint.h> // for uint8_t
+#include <stdint.h> // for uint32_t
 #include <stdio.h> // for printf
 
+uint32_t my_ntohl(uint32_t n){
+    uint32_t n1 = (n & 0x000000ff) << 24;
+    uint32_t n2 = (n & 0x0000ff00) << 8;
+    uint32_t n3 = (n & 0x00ff0000) >> 8;
+    uint32_t n4 = (n & 0xff000000) >> 24;
+    return n1 | n2 | n3 | n4;
+}
+
 int main(int argc, char**argv) {
-	uint32_t var1, var2;
+	uint32_t var1, var2, sum;
 	size_t result;
-	char buf[4];
 
     if(argc != 3){
         printf("You should give 3 arguments.\n");
-        exit(100);
+        return 100;
     }
 
 	FILE *file1, *file2;
     if(!(file1=fopen(argv[1], "r"))) {
         printf("Can't open the first file.\n");
-        exit(101);
+        return 101;
     }
 
     if(!(file2=fopen(argv[2], "r"))) {
         printf("Can't open the second file.\n");
-        exit(101);
+        return 101;
     }
 
-	// result = fread(&var1, sizeof(var1), 1, file1);
-	result = fread(buf, sizeof(buf), 1, file1);
-	printf("%d\n", result);
-	printf("%s\n", buf);
-	if(result != sizeof(var1)) {
+	result = fread(&var1, sizeof(var1), 1, file1);
+	if(result != 1) {
 		printf("Can't read uint32_t integer.\n");
-		exit(102);
+		return 102;
 	}
 
 	result = fread(&var2, sizeof(var2), 1, file2);
-	if(result != sizeof(var2)) {
+	if(result != 1) {
 		printf("Can't read uint32_t integer.\n");
-		exit(102);
+		return 102;
 	}
 
-	printf("%d %d\n", var1, var2);
+	var1 = my_ntohl(var1);
+	var2 = my_ntohl(var2);
+	sum = var1 + var2;
 
-    
+	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", var1, var1, var2, var2, sum, sum);
+
+	return 0;
 }
+
